0e40e2a3c4c8fb26598f8b1a243cb52d159caeac
qJay44 <m70155302@yandex.ru>
2022-12-31 11:49:01 +0300

    585

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,8 +1,11 @@
 package com.example.photogallery
 
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,8 +33,13 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         photoGalleryViewModel = ViewModelProvider(this)[PhotoGalleryViewModel::class.java]
-        thumbnailDownloader = ThumbnailDownloader()
-        lifecycle.addObserver(thumbnailDownloader)
+        val responseHandler = Handler(Looper.myLooper()!!)
+        thumbnailDownloader = ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+            val drawable = BitmapDrawable(resources, bitmap)
+            photoHolder.bindDrawable(drawable)
+        }
+        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+
     }
 
     override fun onCreateView(
@@ -39,6 +47,7 @@ class PhotoGalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewLifecycleOwner.lifecycle.addObserver(thumbnailDownloader.viewLifecycleObserver)
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
 
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
@@ -55,9 +64,14 @@ class PhotoGalleryFragment : Fragment() {
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewLifecycleOwner.lifecycle.removeObserver(thumbnailDownloader.viewLifecycleObserver)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        lifecycle.removeObserver(thumbnailDownloader)
+        lifecycle.removeObserver(thumbnailDownloader.fragmentLifecycleObserver)
     }
 
     private class PhotoHolder(itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
--- app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
@@ -1,28 +1,69 @@
 package com.example.photogallery
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Handler
 import android.os.HandlerThread
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import androidx.lifecycle.*
+import com.example.photogallery.api.FlickrFetcher
+import java.util.concurrent.ConcurrentHashMap
 
-class ThumbnailDownloader<in T> : HandlerThread(TAG), DefaultLifecycleObserver {
+class ThumbnailDownloader<in T : Any>(
+    private val responseHandler: Handler,
+    private val onThumbnailDownloaded: (T, Bitmap) -> Unit
+) : HandlerThread(TAG) {
     companion object {
         private const val TAG = "ThumbnailDownloader"
+        private const val MESSAGE_DOWNLOAD = 0
     }
 
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetcher = FlickrFetcher()
     private var hasQuit = false
 
+    val fragmentLifecycleObserver =
+        object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                super.onCreate(owner)
+                Log.i(TAG, "Starting background thread")
+                start()
+                looper
+            }
 
-    override fun onCreate(owner: LifecycleOwner) {
-        super.onCreate(owner)
-        Log.i(TAG, "Starting background thread")
-        start()
-        looper
-    }
+            override fun onDestroy(owner: LifecycleOwner) {
+                super.onDestroy(owner)
+                Log.i(TAG, "Destroying background thread")
+                quit()
+            }
+        }
 
-    override fun onDestroy(owner: LifecycleOwner) {
-        super.onDestroy(owner)
-        Log.i(TAG, "Destroying background thread")
-        quit()
+    val viewLifecycleObserver =
+        object : DefaultLifecycleObserver {
+            override fun onDestroy(owner: LifecycleOwner) {
+                super.onDestroy(owner)
+                Log.i(TAG, "Clearing all requests from queue")
+                requestHandler.removeMessages(MESSAGE_DOWNLOAD)
+                requestMap.clear()
+            }
+        }
+
+    @Suppress("UNCHECKED_CAST")
+    @SuppressLint("HandlerLeak")
+    override fun onLooperPrepared() {
+        requestHandler = object : Handler(Looper.myLooper()!!) {
+            override fun handleMessage(msg: Message) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    val target = msg.obj as T
+                    Log.i(TAG, "Got a request for URL: ${requestMap[target]}")
+                    handleRequest(target)
+                }
+            }
+
+        }
     }
 
     override fun quit(): Boolean {
@@ -30,7 +71,21 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), DefaultLifecycleObserver {
         return super.quit()
     }
 
+    private fun handleRequest(target: T) {
+        val url = requestMap[target] ?: return
+        val bitmap = flickrFetcher.fetchPhoto(url) ?: return
+        responseHandler.post(Runnable {
+            if (requestMap[target] != url || hasQuit) {
+                return@Runnable
+            }
+            requestMap.remove(target)
+            onThumbnailDownloaded(target, bitmap)
+        })
+    }
+
     fun queueThumbnail(target: T, url: String) {
         Log.i(TAG, "queueThumbnail: Got a URL: $url")
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
     }
 }
